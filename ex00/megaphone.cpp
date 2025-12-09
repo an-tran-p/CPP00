37b52d1a3019b56c98b2d6448e70abf674810479
antran <antranphuong97@gmail.com>
2025-12-09 12:17:28 +0200

    ex00 cpp file

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,39 @@
 /*   By: atran <atran@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/09 11:33:05 by atran             #+#    #+#             */
-/*   Updated: 2025/12/09 11:33:09 by atran            ###   ########.fr       */
+/*   Updated: 2025/12/09 12:15:19 by atran            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <cctype>
+#include <iostream>
+
+void	print_arg(char *arg)
+{
+	while (*arg)
+	{
+		if (std::isalpha(*arg))
+			std::cout << (char)std::toupper(*arg);
+		else
+			std::cout << *arg;
+		arg++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int i = 1;
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		while (i < argc)
+		{
+			if (argv[i])
+				print_arg(argv[i]);
+			i++;
+		}
+		std::cout << std::endl;
+	}
+	return (0);
+}
